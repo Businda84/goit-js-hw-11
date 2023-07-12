7413e71d45d3a6f7e5f3676207a0c31d3356795e
Businda84 <109301774+Businda84@users.noreply.github.com>
2023-07-12 23:00:46 +0300

    hw 11

--- src/index.js
+++ src/index.js
@@ -1,60 +1,62 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
-import { placeholderAPI } from './JSONPlaceholdeAPI';
-import { createMarkup } from './markup';
-const searchBtnEl = document.querySelector('.search-btn');
-const searchQueryEl = document.getElementsByName('searchQuery');
+import { requestAPI, searchByTag } from './requestAPI';
+
 const btnLoadMoreEl = document.querySelector('load-more');
 const searchFormEl = document.querySelector('#search-form');
+const searchQueryEl = document.getElementsByName('searchQuery');
 const postsWrapperEl = document.querySelector('.js-posts');
-const galleryEl = document.querySelector('.gallery-section');
-const totalHits = '';
-
-const placeholderInstance = new placeholderAPI();
-placeholderInstance
-  .fetchPhoto()
-  .then(data => {
-    console.log(data.hits);
-    postsWrapperEl.innerHTML = createMarkup();
-  })
-  .catch(err => {
-    console.log(err);
-  });
-
-searchFormEl.addEventListener('submit', searchImgByTag);
+const galleryEl = document.querySelector('.gallery');
+let page = 1;
 
 function searchImgByTag(event) {
   event.preventDefault();
   const { searchQuery } = event.currentTarget.elements;
-  SearchPhoto = searchQuery.value.trim().toLowerCase();
-  // console.log(SearchPhoto);
-  if (SearchPhoto === '') {
-    Notiflix.info('Enter your request!');
-    return;
-  }
-  console.log(SearchPhoto);
-  getImgByTeg().then(res => {
-    const data = res.data;
-    console.log(res.data);
-    return data;
-  });
+  tag = searchQuery.value.trim();
+  searchQuery.value = '';
+  console.log(tag);
+
+  requestAPI()
+    .then(data => {
+      galleryEl.innerHTML = createMarkup(data);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+function createMarkup(data) {
+  return data
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        views,
+        comments,
+        downloads,
+        likes,
+      }) =>
+        `<div class="photo-card">
+    <a href="${largeImageURL}">
+      <img class="image" src="${webformatURL}" alt="${tags}" loading="lazy"/>
+      <div class="info">
+        <p class="info-item">
+          <b>Likes: ${likes}</b>
+        </p>
+        <p class="info-item">
+          <b>Views: ${views}</b>
+        </p>
+        <p class="info-item">
+          <b>Comments: ${comments}</b>
+        </p>
+        <p class="info-item">
+          <b>Downloads: ${downloads}</b>
+        </p>
+      </div>
+    </a>
+  </div>`
+    )
+    .join('');
 }
 
-const getImgByTeg = async tag => {
-  try {
-    const response = await axios.get(`${BASE_URL}`, options);
-    return console.log(response);
-  } catch {
-    Notiflix.Notify.failure(err.massege);
-  }
-};
-
-let imgData = response.data.hits.map(hit => {
-  webformatURL: hit.webformatURL;
-  largeImageURL: hit.largeImageURL;
-  comments: hit.comments;
-  views: hit.views;
-  downloads: hit.downloads;
-  likes: hit.likes;
-  tags: hit.tags;
-});
+searchFormEl.addEventListener('submit', searchImgByTag);
