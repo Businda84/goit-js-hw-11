dca276766f40f9f8d6e6a9f1669c51a56a3586db
Businda84 <109301774+Businda84@users.noreply.github.com>
2023-07-13 22:04:24 +0300

    HW 11

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import * as basicLightbox from 'basiclightbox';
 import { requestAPI } from './requestAPI';
 
-const btnLoadMoreEl = document.querySelector('load-more');
+const btnLoadMoreEl = document.querySelector('.load-more');
 const searchFormEl = document.querySelector('#search-form');
 const searchQueryEl = document.getElementsByName('searchQuery');
 const postsWrapperEl = document.querySelector('.js-posts');
 const galleryEl = document.querySelector('.gallery');
 let page = 1;
 let tag;
+btnLoadMoreEl.style.display = 'none';
+// const instance = basicLightbox.create(`
+//     <img src="assets/images/image.png" width="800" height="600">
+// `);
+
+// instance.show();
+console.log(btnLoadMoreEl);
 
 function searchImgByTag(event) {
   event.preventDefault();
@@ -17,26 +26,28 @@ function searchImgByTag(event) {
 
   tag = searchQuery.value.trim();
   console.log(tag);
-  if (!tag) {
-    return;
-  }
 
   let searchByTag = '';
 
   searchByTag = tag;
   console.log(tag);
-
+  galleryEl.innerHTML = '';
+  searchQuery.value = '';
   requestAPI(searchByTag)
     .then(data => {
       console.log(data);
-      if (!data) {
-        return console.log('Images not found');
+      if (data.totalHits === 0) {
+        return Notiflix.Notify.failure(
+          "We're sorry, but nothing was found for your search."
+        );
+        btnLoadMoreEl.style.display = 'none';
       }
 
+      btnLoadMoreEl.style.display = 'block';
       createMarkup(data.hits);
     })
     .catch(err => {
-      console.log(err);
+      console.log(err.message);
     });
 }
 
@@ -78,4 +89,23 @@ function createMarkup(data) {
   galleryEl.insertAdjacentHTML('beforeend', markup);
 }
 
+const handlClick = async () => {
+  page += 1;
+  try {
+    if (page === data.totalHits) {
+      Notiflix.Notify.failure(
+        "We're sorry, but you've reached the end of search results."
+      );
+      btnLoadMoreEl.style.display = 'none';
+    }
+
+    galleryEl.insertAdjacentHTML('beforeend', createMarkup(data));
+  } catch {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    btnLoadMoreEl.style.display = 'none';
+  }
+};
 searchFormEl.addEventListener('submit', searchImgByTag);
+btnLoadMoreEl.addEventListener('click', handlClick);
