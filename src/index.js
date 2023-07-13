c912187db1ef6a035001de683f901b307802c220
Businda84 <109301774+Businda84@users.noreply.github.com>
2023-07-13 20:11:07 +0300

    HW11

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
-import { requestAPI, searchByTag } from './requestAPI';
+import * as basicLightbox from 'basiclightbox';
+import { requestAPI } from './requestAPI';
 
 const btnLoadMoreEl = document.querySelector('load-more');
 const searchFormEl = document.querySelector('#search-form');
@@ -8,55 +9,73 @@ const searchQueryEl = document.getElementsByName('searchQuery');
 const postsWrapperEl = document.querySelector('.js-posts');
 const galleryEl = document.querySelector('.gallery');
 let page = 1;
+let tag;
 
 function searchImgByTag(event) {
   event.preventDefault();
   const { searchQuery } = event.currentTarget.elements;
+
   tag = searchQuery.value.trim();
-  searchQuery.value = '';
+  console.log(tag);
+  if (!tag) {
+    return;
+  }
+
+  let searchByTag = '';
+
+  searchByTag = tag;
   console.log(tag);
 
-  requestAPI()
+  requestAPI(searchByTag)
     .then(data => {
-      galleryEl.innerHTML = createMarkup(data);
+      console.log(data);
+      if (!data) {
+        return console.log('Images not found');
+      }
+
+      createMarkup(data.hits);
     })
     .catch(err => {
       console.log(err);
     });
 }
+
 function createMarkup(data) {
-  return data
+  const markup = data
     .map(
       ({
         webformatURL,
-        largeImageURL,
         tags,
+        largeImageURL,
         views,
         comments,
         downloads,
         likes,
-      }) =>
-        `<div class="photo-card">
-    <a href="${largeImageURL}">
-      <img class="image" src="${webformatURL}" alt="${tags}" loading="lazy"/>
-      <div class="info">
-        <p class="info-item">
-          <b>Likes: ${likes}</b>
-        </p>
-        <p class="info-item">
-          <b>Views: ${views}</b>
-        </p>
-        <p class="info-item">
-          <b>Comments: ${comments}</b>
-        </p>
-        <p class="info-item">
-          <b>Downloads: ${downloads}</b>
-        </p>
-      </div>
-    </a>
-  </div>`
+      }) => {
+        const card = `<div class="photo-card">
+      <a href="${largeImageURL}">
+        <img class="image" src="${webformatURL}" alt="${tags}" loading="lazy"/>
+        <div class="info">
+          <p class="info-item">
+            <b>Likes: ${likes}</b>
+          </p>
+          <p class="info-item">
+            <b>Views: ${views}</b>
+          </p>
+          <p class="info-item">
+            <b>Comments: ${comments}</b>
+          </p>
+          <p class="info-item">
+            <b>Downloads: ${downloads}</b>
+          </p>
+        </div>
+      </a>
+    </div>`;
+        return card;
+      }
     )
     .join('');
+  galleryEl.insertAdjacentHTML('beforeend', markup);
 }
 
 searchFormEl.addEventListener('submit', searchImgByTag);
--- src/requestAPI.js
+++ src/requestAPI.js
@@ -3,10 +3,24 @@ import axios from 'axios';
 const API_KEY = '38109927-58adaea0db8d57035fe4e4e71';
 const BASE_URL = 'https://pixabay.com/api/';
 let page = 1;
-let searchByTag = '';
 
-const requestAPI = async () => {
+// const requestAPI = async searchByTag => {
+//   try {
+//     console.log(searchByTag, 'test');
+//     const response = await fetch(
+//       `${BASE_URL}?key=${API_KEY}&q=${searchByTag}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`
+//     );
+//     const photos = await response.json();
+//     // console.log(photos);
+//     return photos;
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// };
+
+export async function requestAPI(searchByTag) {
   try {
+    console.log(searchByTag, 'test');
     const response = await fetch(
       `${BASE_URL}?key=${API_KEY}&q=${searchByTag}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`
     );
@@ -16,6 +30,4 @@ const requestAPI = async () => {
   } catch (error) {
     console.log(error.message);
   }
-};
-
-export { requestAPI, searchByTag };
+}
